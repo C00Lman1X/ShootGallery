6d8993d2233bb99e76b7c4e5772881163fbfaa7b
Krumeckiy <krum666@rambler.ru>
2018-03-06 00:11:21 +0300

    Добавлен вылет гильзы

---
+++ Assets/Scripts/CylinderMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderMovement : MonoBehaviour {
+
+    private bool startMovement;
+    public Transform _transform;
+
+
+    private void FixedUpdate()
+    {
+        if (!startMovement && _transform != null)
+        {
+            float x = (0.5f + Random.Range(-0.1f, 0.1f));
+            float y = 0.5f;
+
+            Vector3 dir = (_transform.up * y + _transform.right * x) * 5;
+            GetComponent<Rigidbody>().AddForce(dir, ForceMode.Impulse);
+            startMovement = true;
+
+            transform.Rotate(new Vector3(90, 0, 0));
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/CylinderSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderSpawn : MonoBehaviour {
+
+    public Transform cylinder;
+    //public CylinderMovement cylinderMovementCS;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+
+       // Vector3 pos = transform.position + transform.forward;
+		if(Input.GetMouseButtonDown(0))
+        {
+            Transform CylinderInstance = (Transform) Instantiate(cylinder, GameObject.Find("CylinderSpawnPoint").transform.position, Quaternion.identity);
+            //GameObject cylinder = Instantiate(GameObject.Find("CylinderSpawnPoint"), pos, Quaternion.identity) as GameObject;
+            CylinderMovement cylinderMovementCS = cylinder.GetComponent<CylinderMovement>();
+            //cylinderMovementCS._transform = transform;
+            //cylinder.transform.up = -transform.forward;
+
+            
+
+
+        }
+	}
+}
